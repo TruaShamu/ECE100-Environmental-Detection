a26e22f0b8b2d4275d84e26cd54fe825c49b95fa
Trua Shamu <sofia_mit@hotmail.com>
2022-11-11 12:34:05 -0600

    Create secrets.h

---
+++ thingsspeak/secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "ECE 100 2.4 GHz"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "$Sh326%eCE100!"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 1921499			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "YW9QEYQIK2W48U0R"   // replace XYZ with your channel write API Key
