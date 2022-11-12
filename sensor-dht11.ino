5995cbac4e7d32615834f26f8ca22321fe942b32
Trua Shamu <sofia_mit@hotmail.com>
2022-11-11 20:52:46 -0600

    Update sensor-dht11.ino

--- sensor-dht11.ino
+++ sensor-dht11.ino
@@ -1,15 +1,11 @@
 #include<dht.h>
 dht DHT;
 
-// if you require to change the pin number, Edit the pin with your arduino pin.
-
 #define DHT11_PIN 3
 
 void setup() {
 
 Serial.begin(9600);
-
-Serial.println("welcome to TechPonder Humidity and temperature Detector"); }
 
 void loop() { // READ DATA
 
